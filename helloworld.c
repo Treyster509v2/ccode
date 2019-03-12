fe6147952e6b15d5f7a6d77d4592018fe3041cb4
trey <tcgtreyster509@gmail.com>
2019-03-12 13:27:23 -0700

    AA

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+//hello.c
+//compile gcc .hello.c
+//run ./a.out
+#include <studio.h>
+int main(){
+printf("*** Hello World *** *\n");
+printf("*       from        *\n");
+printf("*       from        *\n");
+printf("* TT             TT *\n");
+printf("* T     TTT  T      *\n");
+printf("*   T        TT   T *\n");
+printf("*********************\n");
+return 0;
+}
