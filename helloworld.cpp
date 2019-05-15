418a816c0fd7abab942c9b5db17719b76fbea5f4
trey <tcgtreyster509@gmail.com>
2019-05-15 12:11:19 -0700

    AA

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,11 @@
 // helloworld.cpp source code
-#include <studio.h>
+#include <iostream>
 using namespace std;
 
 int main () {
     cout<<"ABCDEFGHIJKLMNOPQRSTUVWXYZ"<<endl;
     cout<<"StevePi"<<endl;
     cout<<"ZULU"<<endl;
+    for (int i = 0 ; i < 256 ; i++)
+    return 0;
 }
